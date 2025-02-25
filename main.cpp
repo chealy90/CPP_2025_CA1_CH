3e2c1cdabe2e6b06dbc34c52ba53b390c95621d0
chealy90 <christopherhealy20@gmail.com>
2025-02-25 00:34:28 +0000

    data is parsed, and organised into vector of structs

--- main.cpp
+++ main.cpp
@@ -1,18 +1,81 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
+void parseLine();
+void readFile();
 
-struct book
+struct Book
 {
     int bookID;
     string title;
-
-
-
+    string author;
+    int release_year;
+    float rating;
+    float copies_sold;
 };
 
+void parseLine(const string &line, Book &newBook) {
+    string temp;
+    int bookID;
+    string title;
+    string author;
+    int release_year;
+    float rating;
+    float copies_sold;
+
+    stringstream ss(line);
+    //id
+    getline(ss, temp, ',');
+    bookID = stoi(temp);
+
+    //title
+    getline(ss, title, ',');
+
+    //author
+    getline(ss, author, ',');
+
+    //release year
+    getline(ss, temp, ',');
+    release_year = stoi(temp);
+
+    //rating
+    getline(ss, temp, ',');
+    rating = stof(temp);
+
+    //copies sold
+    getline(ss, temp, ',');
+    copies_sold = stof(temp);
+
+
+    //fill struct
+    newBook.bookID = bookID;
+    newBook.title = title;
+    newBook.author = author;
+    newBook.release_year = release_year;
+    newBook.rating = rating;
+    newBook.copies_sold = copies_sold;
+}
+
+void readFile() {
+    ifstream fin("books.csv");
+    string nextline;
+
+    if (fin) {
+        while (!fin.eof()) {
+            getline(fin, nextline);
+            Book newBook;
+            parseLine(nextline, newBook);
+        }
+    }
+    fin.close();
+}
+
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    readFile();
     return 0;
 }
