439c139c336d163899f4029f767bb4f663d7c02e
chealy90 <christopherhealy20@gmail.com>
2025-02-25 02:01:20 +0000

    table to display books created, some formatting issues remain

--- main.cpp
+++ main.cpp
@@ -2,10 +2,13 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <vector>
 
 using namespace std;
+struct Book;
 void parseLine();
-void readFile();
+void loadBooks();
+void displayRow();
 
 struct Book
 {
@@ -16,6 +19,8 @@ struct Book
     float rating;
     float copies_sold;
 };
+
+
 
 void parseLine(const string &line, Book &newBook) {
     string temp;
@@ -59,7 +64,7 @@ void parseLine(const string &line, Book &newBook) {
     newBook.copies_sold = copies_sold;
 }
 
-void readFile() {
+void loadBooks(vector<Book> &books) {
     ifstream fin("books.csv");
     string nextline;
 
@@ -68,14 +73,72 @@ void readFile() {
             getline(fin, nextline);
             Book newBook;
             parseLine(nextline, newBook);
+
+            books.push_back(newBook);
         }
     }
     fin.close();
 }
 
 
+void displayRow(const Book &book) {
+    //id is 8 chars after space
+    cout << "| " << book.bookID << string(8 - to_string(book.bookID).size() , ' ');
+
+    //title is 40 chars after space
+    cout << "| " << book.title << string(40 - book.title.size(), ' ');
+
+    //author
+    cout << "| " << book.author << string(26 - book.author.size(), ' ');
+
+    //year
+    cout << "| " << book.release_year << string(10, ' ');
+
+    //rating
+    //adapted from https://www.geeksforgeeks.org/rounding-floating-point-number-two-decimal-places-c-c/
+    int ratingTemp = (int) book.rating * 100;
+    int ratingStringLen = to_string((float) ratingTemp / 100).size();
+
+    cout << "| " << book.rating << string(4, ' ');
+
+    //copies sold
+    cout << "| " << book.copies_sold << string(7, ' ') << "|" << endl;
+
+
+}
+
+void displayTable(const vector<Book> &books) {
+    /*
+    float num = 9.7;
+    float ratingTemp = num * 100;
+    int ratingStringLen = to_string((float) ratingTemp / 100).size();
+    cout << ratingStringLen << endl;
+    */
+
+
+    //HEADERS
+    //give headers lengths to match max expected data
+    //total table width is 122 chars
+    cout << string(119, '-') << endl;
+    cout << "| Book ID | " << "Title" << string(35, ' ') << "| Author" << string(20, ' ') << "| Year Released | Rating | Copies Sold" << endl;
+    cout << string(119, '-') << endl;
+
+    //ROWS
+    for (vector<Book>::const_iterator iter = books.cbegin() ; iter != books.cend() ; iter++) {
+        displayRow(*iter);
+    }
+
+
+    //FINAL ROW LINE
+    cout << string(119, '-') << endl;
+}
+
+
+
 
 int main() {
-    readFile();
+    vector<Book> books;
+    loadBooks(books);
+    displayTable(books);
     return 0;
 }
